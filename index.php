24b10de47608bb86e47841e85d6a508745f2ad1c
Nahid Bin Azhar <nahid.dns@gmail.com>
2015-11-27 02:14:37 +0600

    add multiple items

--- index.php
+++ index.php
@@ -6,15 +6,25 @@ $json=new JSONX('database/db.json');
 // 	echo "successful";
 // }
 
-/*$items=['id'=>10, 'cat'=>'shoe', 'name'=>'Bata-Sleper'];
+$items=[
+	['id'=>1, 'cat'=>'shirt', 'name'=>'Half-Red-L'],
+	['id'=>2, 'cat'=>'shirt', 'name'=>'Half-Red-M'],
+	['id'=>3, 'cat'=>'shirt', 'name'=>'Half-Blue-M'],
+	['id'=>4, 'cat'=>'pant', 'name'=>'Jeans-30'],
+	['id'=>5, 'cat'=>'pant', 'name'=>'Jeans-35'],
+	['id'=>6, 'cat'=>'t-shirt', 'name'=>'Winter-full-M'],
+	['id'=>7, 'cat'=>'t-shirt', 'name'=>'Summer-half-M'],
+	['id'=>8, 'cat'=>'shirt', 'name'=>'Thiny-Full-Checked-L'],
+	['id'=>9, 'cat'=>'pant', 'name'=>'Gabardean-L']
+];
 
 if($json->node('items')->save($items)){
 	echo 'Saved';
-}*/
+}
 
 //var_dump($json->node('products')->where('name', '=', 'Keyboard')->fetch());
 
-$shirts=$json->node('items')->where('id', '=', 10)->fetch();
+$shirts=$json->node('items')->where('id', '=', 5)->fetch();
 
 foreach($shirts as $shirt){
 	echo '<li>'.$shirt['name'].'</li>';
