28bfbf5e54b5224b1fadf84f0e63a0bb92a8e7a0
Plabon <obi.plabon@gmail.com>
2016-04-06 16:30:44 +0600

    added composite condition support

--- index.php
+++ index.php
@@ -18,14 +18,20 @@ $items=[
 	['id'=>9, 'cat'=>'pant', 'name'=>'Gabardean-L']
 ];
 
-if($json->node('items')->save($items)){
-	echo 'Saved';
-}
+// if($json->node('items')->save($items)){
+// 	echo 'Saved';
+// }
 
-//var_dump($json->node('products')->where('name', '=', 'Keyboard')->fetch());
+// var_dump($json->node('products')->where('name', '=', 'Keyboard')->fetch());
 
-$shirts=$json->node('items')->where('id', '=', 5)->fetch();
+// $shirts=$json->node('items')->where('id', '=', 5)->fetch();
 
-foreach($shirts as $shirt){
-	echo '<li>'.$shirt['name'].'</li>';
-}
+// foreach($shirts as $shirt){
+// 	print_r($shirt);
+// }
+
+
+$shirts=$json->node('items')->where(array(array('cat', '=', 'pant'),array('name', '=', 'Gabardean-L'),array('id', '<', 4)))->fetch();
+
+echo '<pre>';
+print_r( $json );
--- jsonx.php
+++ jsonx.php
@@ -8,23 +8,25 @@ class JSONX
 	protected $_key='';
 	protected $_value='';
 	protected $_condition=null;
+
+	protected $_isComposite = false;
+	protected $_compositeConditions = array();
+
 	protected $_conditions=[
 		'>'=>'greater',
 		'<'=>'less',
 		'='=>'equal',
+		'!='=>'notequal',
 		'>='=>'greaterequal',
 		'<='=>'lessequal',
 		];
 
 
-
-
-/*
-	this constructor set main json file path
-	otherwise create it and read file contents
-	and decode as an array and store it in $this->_data
-*/
-
+	/*
+		this constructor set main json file path
+		otherwise create it and read file contents
+		and decode as an array and store it in $this->_data
+	*/
 	function __construct($path)
 	{
 		if(!file_exists($path)){
@@ -52,13 +54,37 @@ class JSONX
 
 	public function where($key=null, $condition=null, $value=null)
 	{
+		if(is_array($key)) {
+			$this->_isComposite = true;
+			$this->whereComposite($key);
+		}
+
+		$this->whereComposite($key, $condition, $value);
+
+		return $this;
+	}
+
+	private function whereSimple($key=null, $condition=null, $value=null)
+	{
 		if(is_null($key) || is_null($condition) || is_null($value)) return false;
 		$this->_key=$key;
 		$this->_condition=$condition;
 		$this->_value=$value;
+	}
 
-		return $this;
+	private function whereComposite(array $conditions)
+	{
+		foreach($conditions as $condition) {
+			if(!isset($condition[0],$condition[1],$condition[2])) continue;
+			
+			$map = array(
+				'key' => $condition[0],
+				'condition' => $condition[1],
+				'value' => $condition[2],
+				);
 
+			$this->_compositeConditions[] = $map;
+		}
 	}
 
 	public function fetch()
@@ -70,43 +96,74 @@ class JSONX
 	    foreach($path as $val){
 
 	    	if(!isset($data[$val])){
-					$terminate=true;
-					break;
+				$terminate=true;
+				break;
 	    	}
-	    		$data=$data[$val];
 
-
+	    	$data=$data[$val];
 	    }
 
-
-	    if(!is_null($this->_condition) || $this->_condition!='')
-	    {
-	    	$func ='where'. ucfirst($this->_conditions[$this->_condition]);
-	    	return $this->$func($data, $this->_key, $this->_value);
+	    if($this->_isComposite) {
+	    	return $this->fetchComposite($data);
+	    } else {
+	    	return $this->fetchSimple($data);
 	    }
 
-
-
-			if($terminate) return false;
-			return $data;
+		if($terminate) return false;
+		return $data;
 	}
 
+	private function fetchSimple($data)
+	{
+		if(is_null($this->_condition) || $this->_condition =='')
+	    	return array();
+	    
+	    $func ='where'. ucfirst($this->_conditions[$this->_condition]);
+	    return $this->$func($data, $this->_key, $this->_value);
+	}
 
+	private function fetchComposite($data)
+	{
+		$filteredData = array();
+		foreach($this->_compositeConditions as $condition) {
+			$func ='where'. ucfirst($this->_conditions[$condition['condition']]);
+			$filteredData[] = $this->$func($data, $condition['key'], $condition['value']);
+		}
+		if(count($filteredData) > 1) {
+			return $this->intersection($filteredData, $data);
+		}
+		return $filteredData;
+	}
 
+	private function intersection(array $filteredData, $nodeData) {
+		$first=array_keys($filteredData[0]);
+		$second=array_keys($filteredData[1]);
+		$next = [];
+		for($i=1; $i<=(count($filteredData)-1); $i++){
+			$next = array_intersect($first, $second);
+			$first=$next;
+			if(count($filteredData)>($i+1))
+				$second=array_keys($filteredData[$i+1]);
+		}
 
-/*
-saveData()
+		$data=[];
+		foreach($next as $key){
+			$data[]=$nodeData[$key];
+		}
+		return $data;
+	}
 
-This function helps to you to save or update data or value in specific node
+	/*
+	saveData()
 
-@param 		:	string $node // ':' colon separeted string
-@param 		: 	string/int $value
-@param 		: 	boolean $array
+	This function helps to you to save or update data or value in specific node
 
-@return 	: 	json otherwise false
-*/
+	@param 		:	string $node // ':' colon separeted string
+	@param 		: 	string/int $value
+	@param 		: 	boolean $array
 
-
+	@return 	: 	json otherwise false
+	*/
 	public function save($value, $array=false)
 	{
 		$json='';
@@ -183,50 +240,50 @@ This method helps to you to find or get specific node value.
 
 	protected function whereGreater($data, $key, $value)
 	{
-			return array_filter($data, function($var) use($key, $value){
-				if(isset($var[$key]))
-				if($var[$key]>$value){
-					return $var;
-				}
-			});
+		return array_filter($data, function($var) use($key, $value){
+			if(isset($var[$key]))
+			if($var[$key]>$value){
+				return $var;
+			}
+		});
 	}
 
 	protected function whereLess($data, $key, $value)
 	{
-			return array_filter($data, function($var) use($key, $value){
-				if(isset($var[$key]))
-				if($var[$key]==$value){
-					return $var;
-				}
-			});
+		return array_filter($data, function($var) use($key, $value){
+			if(isset($var[$key]))
+			if($var[$key]<$value){
+				return $var;
+			}
+		});
 	}
 
 	protected function whereEqual($data, $key, $value)
 	{
-			return array_filter($data, function($var) use($key, $value){
-				if(isset($var[$key]))
-				if($var[$key]==$value){
-					return $var;
-				}
-			});
+		return array_filter($data, function($var) use($key, $value){
+			if(isset($var[$key]))
+			if($var[$key]==$value){
+				return $var;
+			}
+		});
 	}
 
 	protected function whereGreaterequal($data, $key, $value)
 	{
-			return array_filter($data, function($var) use($key, $value){
-				if(isset($var[$key]))
-				if($var[$key]>=$value){
-					return $var;
-				}
-			});
+		return array_filter($data, function($var) use($key, $value){
+			if(isset($var[$key]))
+			if($var[$key]>=$value){
+				return $var;
+			}
+		});
 	}
 	protected function whereLessequal($data, $key, $value)
 	{
-			return array_filter($data, function($var) use($key, $value){
-				if(isset($var[$key]))
-				if($var[$key]<=$value){
-					return $var;
-				}
-			});
+		return array_filter($data, function($var) use($key, $value){
+			if(isset($var[$key]))
+			if($var[$key]<=$value){
+				return $var;
+			}
+		});
 	}
 }
